76faeba6023208410d8ce6c5459cd357dff38fed
Prateek Agarawal <prateekagarawal@Mac.attlocal.net>
2025-02-12 06:46:36 -0800

    Add main

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math"
+	"net/http"
+	"strings"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/google/uuid"
+)
+
+
+
+func main() {
+
+}
